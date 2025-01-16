a96e372716eee8b8105f5247908d0e44e555b0d9
Ameya Londhe <ameya.londhe@ad.infosys.com>
2025-01-16 16:01:49 +0530

    initial commit

---
+++ src/main/java/com/project/medialytics/MedialyticsApplication.java
@@ -0,0 +1,13 @@
+package com.project.medialytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedialyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedialyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/medialytics/MedialyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medialytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedialyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
